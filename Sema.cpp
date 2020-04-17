e527405df4d43ab83b265febb972b98072033b6e
gautam3000 <58000693+gautam3000@users.noreply.github.com>
2020-04-17 17:47:25 +0530

    Update Sema.cpp

--- Sema.cpp
+++ Sema.cpp
@@ -7,62 +7,62 @@
 using namespace std;
 
 int resources = 5;
-sem_t semaphor;
+sem_t sema;
 
-void * F1(void *) {
+void * f1(void *) {
     int a;
-    sem_getvalue(&semaphor, &a);
+    sem_getvalue(&sema, &a);
     cout << "Semaphore F1 = " << a << endl << endl;
     while (a <= 0)
         ;
 
-    cout << "Lock obtain, in F1" << endl
+    cout << "Lock acquired , in f1" << endl
          << endl;
-    sem_wait(&semaphor);
+    sem_wait(&sema);
     resources++;
     sleep(2);
-    sem_post(&semaphor);
-    cout << "Lock free on semaphore, in f1" << endl;
-    cout << "Current value  is " << resources << ", in F1" << endl
+    sem_post(&sema);
+    cout << "Lock released, in f1" << endl;
+    cout << "Current value is " << resources << ", in f1" << endl;
 }
 
-void * F2(void *) {
+void * f2(void *) {
     int b;
-    sem_getvalue(&semaphor, &b);
+    sem_getvalue(&sema, &b);
     cout << "Semaphore F2 = " << b << endl << endl;
     while (b <= 0)
         ;
-    cout << "Lock obtain, in F2" << endl
+    cout << "Lock acquired, in f2" << endl
          << endl;
-    sem_wait(&semaphor);
+    sem_wait(&sema);
     resources--;
     sleep(2);
-    sem_post(&semaphor);
-    cout << "Lock free on semaphore, in f2" << endl;
+    sem_post(&sema);
+    cout << "Lock released, in f2" << endl;
     cout << "Current value is " << resources << ", in f2" << endl;
     
 }
 int main() {
     pthread_t thread1, thread2;
-    sem_init( & semaphor, 0, 1);
+    sem_init( & sema, 0, 1);
     int sg;
-    sem_getvalue(&semaphor, &sg);
-    cout << "Current value of the semaphore = " << sg<<endl;
+    sem_getvalue(&sema, &sg);
+    cout << "Current value  = " << sg<<endl;
     
     int j = 3;
     while (j--)
     {
-        pthread_create( & thread1, NULL, F1, NULL);
-        pthread_create( & thread2, NULL, F2, NULL);
+        pthread_create( & thread1, NULL, f1, NULL);
+        pthread_create( & thread2, NULL, f2, NULL);
         sleep(5);
         cout << endl
-             << "Loop = " << j << endl
+             << "Loop = " << j-- << endl
              << endl;
     }
 
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
-    cout << "Current value is = " << resources << endl;
+    cout << "Current value of resources = " << resources << endl;
   
     return 0;
 }
