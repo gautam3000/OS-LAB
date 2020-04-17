511ae0efd859022203282f622d29b674533f3dd2
gautam3000 <58000693+gautam3000@users.noreply.github.com>
2020-04-17 17:35:37 +0530

    Update Sema.cpp

--- Sema.cpp
+++ Sema.cpp
@@ -9,37 +9,37 @@ using namespace std;
 int resources = 5;
 sem_t semaphor;
 
-void * firstFunction(void *) {
-    int q;
-    sem_getvalue(&semaphor, &q);
-    cout << "Semaphore F1 = " << q << endl << endl;
-    while (q <= 0)
+void * F1(void *) {
+    int a;
+    sem_getvalue(&semaphor, &a);
+    cout << "Semaphore F1 = " << a << endl << endl;
+    while (a <= 0)
         ;
 
-    cout << "Lock acquired on semaphore, in First Function" << endl
+    cout << "Lock obtain, in F1" << endl
          << endl;
     sem_wait(&semaphor);
     resources++;
     sleep(2);
     sem_post(&semaphor);
-    cout << "Lock released on semaphore, in First Function" << endl;
-    cout << "Current value of resources is " << resources << ", in First Function" << endl;
+    cout << "Lock free on semaphore, in f1" << endl;
+    cout << "Current value  is " << resources << ", in F1" << endl
 }
 
-void * secondFunction(void *) {
-    int p;
-    sem_getvalue(&semaphor, &p);
-    cout << "Semaphore F2 = " << p << endl << endl;
-    while (p <= 0)
+void * F2(void *) {
+    int b;
+    sem_getvalue(&semaphor, &b);
+    cout << "Semaphore F2 = " << b << endl << endl;
+    while (b <= 0)
         ;
-    cout << "Lock acquired on semaphore, in Second Function" << endl
+    cout << "Lock obtain, in F2" << endl
          << endl;
     sem_wait(&semaphor);
     resources--;
     sleep(2);
     sem_post(&semaphor);
-    cout << "Lock released on semaphore, in Second Function" << endl;
-    cout << "Current value of resources is " << resources << ", in Second Function" << endl;
+    cout << "Lock free on semaphore, in f2" << endl;
+    cout << "Current value is " << resources << ", in f2" << endl;
     
 }
 int main() {
@@ -47,22 +47,22 @@ int main() {
     sem_init( & semaphor, 0, 1);
     int sg;
     sem_getvalue(&semaphor, &sg);
-    cout << "Corrent value of the semaphore = " << sg<<endl;
+    cout << "Current value of the semaphore = " << sg<<endl;
     
-    int i = 3;
-    while (i--)
+    int j = 3;
+    while (j--)
     {
-        pthread_create( & thread1, NULL, firstFunction, NULL);
-        pthread_create( & thread2, NULL, secondFunction, NULL);
+        pthread_create( & thread1, NULL, F1, NULL);
+        pthread_create( & thread2, NULL, F2, NULL);
         sleep(5);
         cout << endl
-             << "Loop = " << i << endl
+             << "Loop = " << j << endl
              << endl;
     }
 
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
-    cout << "Current value of resources = " << resources << endl;
+    cout << "Current value is = " << resources << endl;
   
     return 0;
 }
